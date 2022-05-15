25edd33cdb97860c5ff7d8fe9f16f367cfc95327
Binit Gupta <binitguptapro@gmail.com>
2022-05-15 11:30:05 +0530

    15-05-2022

--- app/src/main/java/com/bg/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/bg/tic_tac_toe/MainActivity.java
@@ -2,6 +2,7 @@ package com.bg.tic_tac_toe;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -105,6 +106,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Dark Mode off
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
     }
 
--- app/src/main/java/com/bg/tic_tac_toe/info.java
+++ app/src/main/java/com/bg/tic_tac_toe/info.java
@@ -2,13 +2,42 @@ package com.bg.tic_tac_toe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static com.bg.tic_tac_toe.R.id.copy_button;
 
 public class info extends AppCompatActivity {
+
+    ImageButton button;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        text = findViewById(R.id.email_id);
+        button = findViewById(copy_button);
+
+        //to copy a text
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("Email Address", text.getText().toString());
+                clipboard.setPrimaryClip(clip);
+
+                Toast.makeText(info.this,"Email address copied", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
